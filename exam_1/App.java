7ea23f2567a05caa721a801699a52492fd521ed4
Jhon Gerick <111488392+JhonGerickSamaco@users.noreply.github.com>
2022-09-08 14:29:11 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+        Student me = new Student("Samaco", "Jhon Gerick", 'V', "08/30/2004", 202210741, "samacojv@students.national-u.edu.ph", true);
+        me.sayMyStudentNumber();
+        me.sayMyEmailAddress();
+        me.amIAwesome();
+    }
+}
